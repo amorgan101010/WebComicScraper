bd5d4005cdca928e34bed0c0450f8947764372fd
Aileen Morgan <amorgan101010@protonmail.com>
2025-11-19 16:30:38 -0500

    init commit of my selenium based webcomic downloader

---
+++ Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System.Net.Http.Headers;
+
+IWebDriver driver = new ChromeDriver();
+
+var currentUrl = "https://www.nuklearpower.com/2001/03/02/episode-001-were-going-where/";
+var finalUrl = "https://www.nuklearpower.com/2010/06/01/the-epilogue/";
+
+do
+{
+    driver.Navigate().GoToUrl(currentUrl);
+
+    driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
+
+    var comicDiv = driver.FindElement(By.Id("comic"));
+    var comic = comicDiv.FindElement(By.TagName("img"));
+    var comicUrl = comic.GetAttribute("src");
+    var comicName = comic.GetAttribute("title");
+    await DownloadComicAsync(comicUrl, comicName); // TODO: avoid possible nullref
+}
+while (currentUrl != finalUrl);
+
+static async Task DownloadComicAsync(string comicUrl, string comicName)
+{
+    // adapted from https://www.reddit.com/r/csharp/comments/11r7o1d/comment/jc6zy2g/
+    using HttpClient httpClient = new();
+    byte[] imageBytes = await httpClient.GetByteArrayAsync(comicUrl);
+
+    string projectDir = Directory.GetCurrentDirectory();
+    string localPath = Path.Combine(projectDir, comicName + ".jpg");
+
+    File.WriteAllBytes(localPath, imageBytes);
+}
+
