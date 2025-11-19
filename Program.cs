001d423d654b19d654eb2dcef2bef4857ebff33b
Aileen Morgan <amorgan101010@protonmail.com>
2025-11-19 16:44:26 -0500

    got the webdriving loop locked down

--- Program.cs
+++ Program.cs
@@ -1,17 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
-using System.Net.Http.Headers;
 
 IWebDriver driver = new ChromeDriver();
 
 var currentUrl = "https://www.nuklearpower.com/2001/03/02/episode-001-were-going-where/";
-var finalUrl = "https://www.nuklearpower.com/2010/06/01/the-epilogue/";
+//var finalUrl = "https://www.nuklearpower.com/2010/06/01/the-epilogue/";
+var finalUrl = "https://www.nuklearpower.com/2001/03/14/episode-005-run-heroes-run/";
 
 do
 {
-    driver.Navigate().GoToUrl(currentUrl);
-
+    driver.Navigate().GoToUrl(currentUrl); // TODO: avoid possible nullref
     driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromMilliseconds(500);
 
     var comicDiv = driver.FindElement(By.Id("comic"));
@@ -20,10 +19,14 @@ do
     var comicName = comic.GetAttribute("title");
     await DownloadComicAsync(comicUrl, comicName); // TODO: avoid possible nullref
 
-    //TODO: move on to the next URL
-    currentUrl = finalUrl;
+    var nextButton = driver.FindElement(By.ClassName("navbar-next"));
+    var nextLink = nextButton.FindElement(By.TagName("a"));
+    currentUrl = nextLink.GetAttribute("href");
+
 }
 while (currentUrl != finalUrl);
+
+// TODO: Stitch them into a PDF with the name on each page
 
 static async Task DownloadComicAsync(string comicUrl, string comicName)
 {
